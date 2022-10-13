08069eaa9334850de9b7472c6b2bd7f986d7258b
jhonatanjgz <5825@holbertonstudents.com>
2022-10-13 09:48:08 -0700

    uno a uno los tipos de datos

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,6 +5,15 @@
  */
 int main(void)
 {
-sizeof();
+char tamaño_char=sizeof(char);
+int tamaño_int=sizeof(int);
+long int tamaño_long_int=sizeof(long int);
+long long int tamaño_long_long_int=sizeof(long long int);
+float tamaño_float=sizeof(float); 
+printf("Size of a char: %c");
+ printf("Size of a int: %i");
+ printf("Size of a long int: %li");
+ printf("Size of a long long int: %lli");
+ printf("Size of a float: %f");
 return (0);
 }
