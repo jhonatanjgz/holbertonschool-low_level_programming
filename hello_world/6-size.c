d09038f65edfeafde143c1c39f4c419c59abfa4e
jhonatanjgz <5825@holbertonstudents.com>
2022-10-13 09:57:44 -0700

    betty ready

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,15 +5,15 @@
  */
 int main(void)
 {
-char tamaño_char=sizeof(char);
-int tamaño_int=sizeof(int);
-long int tamaño_long_int=sizeof(long int);
-long long int tamaño_long_long_int=sizeof(long long int);
-float tamaño_float=sizeof(float); 
-printf("Size of a char: %c");
- printf("Size of a int: %i");
- printf("Size of a long int: %li");
- printf("Size of a long long int: %lli");
- printf("Size of a float: %f");
+char size_char = sizeof(char);
+int size_int = sizeof(int);
+long int size_long_int = sizeof(long int);
+long long int size_long_long_int = sizeof(long long int);
+float size_float = sizeof(float);
+printf("Size of a char: %c", size_float);
+printf("Size of a int: %i", size_int);
+printf("Size of a long int: %li", size_long_int);
+printf("Size of a long long int: %lli", size_long_long_int);
+printf("Size of a float: %f", size_float);
 return (0);
 }
