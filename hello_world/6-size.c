099d0430bd8427fc1e1ea0a69cb015787c9fefd7
jhonatanjgz <5825@holbertonstudents.com>
2022-10-13 10:07:23 -0700

    cambiar %c por %d

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,7 +10,7 @@ int size_int = sizeof(int);
 long int size_long_int = sizeof(long int);
 long long int size_long_long_int = sizeof(long long int);
 float size_float = sizeof(float);
-printf("Size of a char: %c byte(s)\n", size_float);
+printf("Size of a char: %d byte(s)\n", size_float);
 printf("Size of an int: %i byte(s)\n", size_int);
 printf("Size of a long int: %li byte(s)\n", size_long_int);
 printf("Size of a long long int: %lli byte(s)\n", size_long_long_int);
