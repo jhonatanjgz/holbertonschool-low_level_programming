bbe36861f64f6a3bfc774a5c082eff6c9b5121eb
jhonatanjgz <5825@holbertonstudents.com>
2022-10-13 09:21:20 -0700

    con comentarios

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
+
+/**
+ * main - imprime en pantalla
+ * Return: 0 siempre
+ */
+
 int main(void)
 {
-  printf ("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
