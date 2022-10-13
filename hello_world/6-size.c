c10e69cfb1b2e89cfe6f75856a48a47a7bd379e1
jhonatanjgz <5825@holbertonstudents.com>
2022-10-13 10:04:01 -0700

    con decimales controlados

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,9 +11,9 @@ long int size_long_int = sizeof(long int);
 long long int size_long_long_int = sizeof(long long int);
 float size_float = sizeof(float);
 printf("Size of a char: %c byte(s)\n", size_float);
-printf("Size of a int: %i byte(s)\n", size_int);
+printf("Size of an int: %i byte(s)\n", size_int);
 printf("Size of a long int: %li byte(s)\n", size_long_int);
 printf("Size of a long long int: %lli byte(s)\n", size_long_long_int);
-printf("Size of a float: %f byte(s)\n", size_float);
+printf("Size of a float: %1.0f byte(s)\n", size_float);
 return (0);
 }
